106ba550ec142015eedbf2b341b4a216aea3a346
Manish_sharma_attri <102635250+TheHemanUltra@users.noreply.github.com>
2022-12-22 15:18:36 +0530

    Add files via upload

---
+++ implementation.py
@@ -0,0 +1,7 @@
+from augmentationScript import *
+
+noOfAugImg = 10
+sourceFolder = 'D:\\DXProjects\\ImageAugmentation\\src'
+destFolder = 'D:\\DXProjects\\ImageAugmentation\\dest'
+
+random_augmented_image_generater(noOfAugImg,sourceFolder,destFolder)
